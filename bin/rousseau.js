1ffb8b124355d173f2e7d9678adeb71abc2b3652
azu <azuciao@gmail.com>
2022-09-10 12:33:24 +0900

    CI: update node versions

--- bin/rousseau.js
+++ bin/rousseau.js
@@ -1,15 +1,15 @@
 #! /usr/bin/env node
 
-var _ = require('lodash');
-var color = require('bash-color');
-var Table = require('cli-table');
-var findLineColumn = require('find-line-column');
+var _ = require("lodash");
+var color = require("bash-color");
+var Table = require("cli-table");
+var findLineColumn = require("find-line-column");
 
-var fs = require('fs');
-var path = require('path');
+var fs = require("fs");
+var path = require("path");
 
-var rousseau = require('../lib');
-var tokenizeHTML = require('tokenize-htmltext');
+var rousseau = require("../lib");
+var tokenizeHTML = require("tokenize-htmltext");
 
 // Colors for levels
 var LEVELS = {
@@ -30,7 +30,7 @@ function parseFile(input) {
     fileContent = fs.readFileSync(input, { encoding: "utf-8" });
     var ext = path.extname(input);
 
-    if (ext == '.html') return tokenizeHTML(fileContent);
+    if (ext == ".html") return tokenizeHTML(fileContent);
 
     return fileContent;
 }
@@ -45,26 +45,39 @@ if (!input) {
 // Read and lint file
 var content = parseFile(input);
 
-rousseau(content, function(err, results) {
+rousseau(content, function (err, results) {
     if (err) {
-        console.log(color.red("Error: "+(err.stack || err)));
+        console.log(color.red("Error: " + (err.stack || err)));
         return process.exit(1);
     }
 
     var levels = {};
     var table = new Table({
-        chars: { 'top': '' , 'top-mid': '' , 'top-left': '' , 'top-right': ''
-         , 'bottom': '' , 'bottom-mid': '' , 'bottom-left': '' , 'bottom-right': ''
-         , 'left': '' , 'left-mid': '' , 'mid': '' , 'mid-mid': ''
-         , 'right': '' , 'right-mid': '' , 'middle': ' ' },
-        style: { 'padding-left': 0, 'padding-right': 0 }
+        chars: {
+            top: "",
+            "top-mid": "",
+            "top-left": "",
+            "top-right": "",
+            bottom: "",
+            "bottom-mid": "",
+            "bottom-left": "",
+            "bottom-right": "",
+            left: "",
+            "left-mid": "",
+            mid: "",
+            "mid-mid": "",
+            right: "",
+            "right-mid": "",
+            middle: " "
+        },
+        style: { "padding-left": 0, "padding-right": 0 }
     });
 
     if (results.length > 0) {
-        console.log("Results for '"+color.wrap(input, "white", "underline")+"':");
+        console.log("Results for '" + color.wrap(input, "white", "underline") + "':");
         console.log("");
 
-        _.each(results, function(result) {
+        _.each(results, function (result) {
             var color = LEVELS[result.level];
             var startPos = findLineColumn(fileContent, result.index);
             var endPos = findLineColumn(fileContent, result.index + result.offset);
@@ -72,17 +85,27 @@ rousseau(content, function(err, results) {
             levels[result.level] = (levels[result.level] || 0) + 1;
 
             table.push([
-                    "  ", startPos.line+":"+startPos.col,
-                    endPos.line+":"+endPos.col,
-                    color("["+result.level+"]"),
-                    result.message
+                "  ",
+                startPos.line + ":" + startPos.col,
+                endPos.line + ":" + endPos.col,
+                color("[" + result.level + "]"),
+                result.message
             ]);
         });
 
         console.log(table.toString());
         console.log("");
-        console.log(color.cyan(results.length+" Problems ("+(levels.error || 0)+" errors, "+(levels.warning || 0)+" warnings)"));
+        console.log(
+            color.cyan(
+                results.length +
+                    " Problems (" +
+                    (levels.error || 0) +
+                    " errors, " +
+                    (levels.warning || 0) +
+                    " warnings)"
+            )
+        );
     } else {
-        console.log(color.green("All good!"))
+        console.log(color.green("All good!"));
     }
 });
